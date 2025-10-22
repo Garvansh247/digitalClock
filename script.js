b6ad2aebb8a6ba0e75392764cda6cdd3ad3b097e
Garvansh <garvansh247@gmail.com>
2025-10-22 10:04:41 +0530

    comment out method 1 and build creative method 2

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const clock = document.querySelector("#clock");
 setInterval(() => {
+    // let date = new Date();
+    // let hours = date.getHours();
+    // let minutes = date.getMinutes();
+    // let seconds = date.getSeconds();
+    // if(seconds<10){
+    //     if(minutes<10)clock.textContent = `${hours}:0${minutes}:0${seconds}`;
+    //     else clock.textContent = `${hours}:${minutes}:0${seconds}`;
+    // }else{
+    //     if(minutes<10)clock.textContent = `${hours}:0${minutes}:${seconds}`;
+    //     else clock.textContent = `${hours}:${minutes}:${seconds}`;
+    // }
+
+
     let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    if(seconds<10){
-        if(minutes<10)clock.textContent = `${hours}:0${minutes}:0${seconds}`;
-        else clock.textContent = `${hours}:${minutes}:0${seconds}`;
-    }else{
-        if(minutes<10)clock.textContent = `${hours}:0${minutes}:${seconds}`;
-        else clock.textContent = `${hours}:${minutes}:${seconds}`;
-    }
+    let hours = String(date.getHours()).padStart(2, '0');
+    let minutes = String(date.getMinutes()).padStart(2, '0');
+    let seconds = String(date.getSeconds()).padStart(2, '0');
+    clock.textContent = `${hours}:${minutes}:${seconds}`;
 }, 1000); // after every 1000 milliseconds the function will be called and the time will be updated
